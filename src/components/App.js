589bbec3fbd3c244f5dee23aa8605be1c3796634
Dr. Lewis <mlewis8@lewisu.edu>
2023-10-29 09:34:44 -0500

    Adding CreateContact function

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
 import { useState, useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import "../css/App.css";
-
-
+import CreateContact from "./CreateContact.js";
+import * as ContactsAPI from "../utils/ContactsAPI";
 const App = () => {
   let navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
+
+  const createContact = (contact) => {
+      const create = async () => {
+        const res = await ContactsAPI.create(contact);
+        setContacts(contacts.concat(res));
+      };
+
+      create();
+      navigate("/");
+  };
+
+  useEffect(() => {
+      const getContacts = async () => {
+        const res = await ContactsAPI.getAll();
+        setContacts(res);
+      };
+
+      getContacts();
+  }, []);
+
+  return (
+    <Routes>
+    
+      <Route
+        path="/create"
+        element={
+          <CreateContact
+            onCreateContact={(contact) => {
+              createContact(contact);
+            }}
+          />
+        }
+      />
+    </Routes>
+  );
 };
 
 export default App;
--- src/utils/ContactsAPI.js
+++ src/utils/ContactsAPI.js
@@ -1,8 +1,8 @@
 const api = process.env.REACT_APP_CONTACTS_API_URL || "http://localhost:5001";
 
 let token = localStorage.token;
-
-if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
+const stringResult = Math.random().toString(36)
+if (!token) token = localStorage.token = stringResult.substring(stringResult.length -8);
 
 const headers = {
   Accept: "application/json",
