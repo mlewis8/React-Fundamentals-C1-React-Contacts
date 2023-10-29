b08b1d2f469ae52297217d4877d9061d5174ee74
Dr. Lewis <mlewis8@lewisu.edu>
2023-10-29 08:40:21 -0500

    Removing three import lines.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 import { useState, useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import "../css/App.css";
-import ListContacts from "./ListContacts";
-import CreateContact from "./CreateContact.js";
-import * as ContactsAPI from "../utils/ContactsAPI";
+
 
 const App = () => {
   let navigate = useNavigate();
